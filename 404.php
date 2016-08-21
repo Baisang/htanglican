b701894e31b1e3ce1521306dedbdc8a44d7c7a74
Baisang <sang.bri@gmail.com>
2016-08-21 02:13:42 -0400

    add 404 page

---
+++ 404.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+<div class="header-image" style="background-image:
+  -webkit-linear-gradient(bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.4)), url('<?php bloginfo('template_directory');?>/images/give_banner.jpg');
+  background-image:
+  linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0),
+    rgba(0, 0, 0, 0.4)
+    ),
+  url('<?php bloginfo('template_directory');?>/images/give_banner.jpg'); background-position: 0% 80%">
+  <div class="super-heading">Page not found</div>
+</div>
+<div class="container-fluid container-pad-5">
+	<div class="row row-center">
+		<div class="col-md-2"></div>
+		<div class="col-md-8">
+      <h2>Oops! Page not found</h2>
+      <p>Sorry, there's nothing here. Try using the navbar above to find the page you were looking for.</p>
+		</div>
+	</div>
+</div>
+
+
+<?php get_footer(); ?>
--- post.php
+++ post.php
@@ -2,7 +2,7 @@
   <div class="col-md-2"></div>
   <div class="col-md-8">
     <h3 class="blog-title"><?php the_title(); ?></h3>
-    <p class="blog-meta"><a href="<?php get_permalink();?>"><?php the_date();?> by <?php the_author(); ?></a></p>
+    <p class="blog-meta"><a href="<?php echo get_permalink();?>"><?php the_date();?> by <?php the_author(); ?></a></p>
     <?php the_content(); ?>
   </div>
 </div>
