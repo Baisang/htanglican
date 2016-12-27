11eb8bc9db0c47895b1d173bd3f8211bf5bd8c4d
Brian Sang <sang.bri@gmail.com>
2016-12-27 12:11:40 -0800

    migrated front page, missing one link

--- front-page.php
+++ front-page.php
@@ -20,7 +20,7 @@
         <div class="carousel-caption">
           <h1>WHAT IS ANGLICANISM?</h1>
           <div class="sub-caption">Anglicanism is an ancient expression of the Christian faith with an exciting future.</div>
-          <p><a class="btn btn-lg btn-primary" href="anglicanism.html" role="button">Learn More</a></p>
+          <p><a class="btn btn-lg btn-primary" href="http://clevelandanglican.com/what-is-anglicanism/" role="button">Learn More</a></p>
         </div>
       </div>
     </div>
@@ -29,9 +29,9 @@
     <div class="third-slide" style="  background-image: linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('<?php bloginfo('template_directory');?>/images/Clevelandsk.jpg');">
       <div class="container">
         <div class="carousel-caption">
-          <h2>HEAR OUR STORY</h2>
+          <h1>HEAR OUR STORY</h1>
           <div class="sub-caption">Discover our church's rich history and exciting future.</div>
-          <p><a class="btn btn-lg btn-primary" href="story.html" target="_blank">Our Story</a></p>
+          <p><a class="btn btn-lg btn-primary" href="http://clevelandanglican.com/our-story" target="_blank">Our Story</a></p>
         </div>
       </div>
     </div>
@@ -55,7 +55,7 @@
   <div class="col-lg-4">
     <span class="glyphicon glyphicon-envelope glyphs"></span>
     <h2>Join our email list</h2>
-    <p> Receive Holy Trinity news updates via email</p>
+    <p> Receive Church news updates via email</p>
 
     <button type="button" class="btn btn-default" data-toggle="modal" data-target="#newsUpdates">
       <div class="market-btn-text">Subscribe &raquo;</div>
@@ -65,7 +65,7 @@
         <div class="modal-content">
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h3 class="modal-title">Sign up for Holy Trinity Newsletter</h3>
+            <h3 class="modal-title">Sign up for Church Newsletter</h3>
           </div>
           <div class="modal-body">
             <!-- Begin MailChimp Signup Form -->
@@ -90,36 +90,12 @@
 
   </div><!-- /.col-lg-4 -->
   <div class="col-lg-4">
-    <span class="glyphicon glyphicon-book glyphs"></span>
-    <h2>Morning Prayer</h2>
-    <p> Get Morning Prayer devotionals daily via email</p>
+    <span class="glyphicon glyphicon-question-sign glyphs"></span>
+    <h2>What to Expect</h2>
+    <p>What can I expect at Transfiguration?</p>
     <button type="button" class="btn btn-default" data-toggle="modal" data-target="#morningPrayer">
-      <div class="market-btn-text">Sign up &raquo;</div>
+      <div class="market-btn-text">See more &raquo;</div>
     </button>
-    <div class="modal fade" id="morningPrayer">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h3 class="modal-title">Sign up for Holy Trinity Morning Prayer</h3>
-          </div>
-          <div class="modal-body">
-            <!-- Begin MailChimp Signup Form -->
-            <div id="mc_embed_signup">
-              <form action="//htanglican.us11.list-manage.com/subscribe/post?u=e0ae89cb20c49a75aed525b35&amp;id=9f5cf135e1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-                <div id="mc_embed_signup_scroll">
-                  <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-                  <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                  <div style="position: absolute; left: -5000px; padding-top: 5em"><input type="text" name="b_e0ae89cb20c49a75aed525b35_9f5cf135e1" tabindex="-1" value=""></div>
-                  <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-                </div>
-              </form>
-            </div>
-            <!--End mc_embed_signup-->
-          </div>
-        </div><!-- /.modal-content -->
-      </div><!-- /.modal-dialog -->
-    </div><!-- /.modal -->
   </div><!-- /.col-lg-4 -->
   <div class="col-lg-4">
     <span class="glyphicon glyphicon-gift glyphs"></span>
@@ -153,7 +129,7 @@
 <div class="row featurette">
   <div class="col-md-7">
     <div class="featurette-heading">Christian Community</div>
-    <p class="lead">Like the early Church, we aspire to regularly gather for fellowship, support, and worship. We envision forging strong spiritual friendships in community groups to sustain us in the changes and chances of life. Our desire is that we would be a community in which our pastor knows his sheep and walks with them as we do life together—not just on Sundays.</p>
+    <p class="lead">Like the early Church, we aspire to regularly gather for fellowship, support, and worship. We envision forging strong spiritual friendships in community groups to sustain us in the changes and chances of life. Our desire is that we would be a community in which our pastors know their sheep and walk with them as we do life together—not just on Sundays.</p>
   </div>
   <div class="col-md-5">
     <img class="featurette-image img-responsive center-block featurette-image1" src="<?php bloginfo('template_directory');?>/images/christiancommunity.jpg" alt="Christian Community">
@@ -180,7 +156,7 @@
     <p class="lead">Like the early Church, we aspire to live not for ourselves but for the good of our neighbors. We envision serving our city for its flourishing by sharing the love of God in both word and deed. We desire to be people who are more concerned with serving than being served—especially for the welfare of the least among us.</p>
   </div>
   <div class="col-md-5">
-    <img class="featurette-image img-responsive center-block featurette-image3" src="<?php bloginfo('template_directory');?>/images/serving_our_city.jpg" alt="serving our city">
+    <img class="featurette-image img-responsive center-block featurette-image3" src="<?php bloginfo('template_directory');?>/images/ministries_banner.jpg" alt="serving our city">
   </div>
 </div>
 
